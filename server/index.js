419b6b05bce21f64a0fc1a3cfe753d200147799f
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-07-31 16:13:13 +0100

    Express server setup :rocket:

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log('listening on port 5000...')
+});   
