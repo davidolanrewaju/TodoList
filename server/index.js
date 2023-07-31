41e6802220a4a88c4d5a1d87bf482caff0ff2828
David <davidolanrewaju538@gmail.com>
2023-07-31 22:24:59 +0100

    Connect postgres to express server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import pool from './db';
 
 const app = express();
 
-//middleware
+// MIDDLEWARE //
 app.use(cors());
 app.use(express.json());
+
+// ROUTES //
+
+//Create Todo
+// app.post("/todos", (req, res) => {
+//     try {
+        
+//     } catch (err) {
+//         console.error(err.message)
+//     }
+// })
+
+//Get todos for display
+
+//Get todo to update or perform CRUD operation on
+
+//Edit or Update todo
+
+//Delete todo
 
 app.listen(5000, () => {
     console.log('listening on port 5000...')
