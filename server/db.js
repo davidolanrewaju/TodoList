2e974730ac7fef317f8923e6e5e43006db3c78f3
David <davidolanrewaju538@gmail.com>
2023-08-01 19:58:25 +0100

    Refactor code into Common JS :arrows_counterclockwise:

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-import { Pool } from "pg";
+const { Pool } = require("pg");
 
 const pool = new Pool({
     user: "postgres",
@@ -8,4 +8,4 @@ const pool = new Pool({
     database: "tododb"
 });
 
-export default pool;
+module.exports = pool;
