8154c3eebfcc55053b5796b1a197b42d160c8be7
David <davidolanrewaju538@gmail.com>
2023-08-02 19:20:02 +0100

    Install packages required for the client side

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-300">
+      Simple React Typescript Tailwind Sample
+    </h1>
+  );  
+}
+
+export default App;
