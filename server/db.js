dd3b4a07eacbadd3b7677b0da72cbe6ff5f57f5f
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-08-04 23:47:19 +0100

    Remove db security

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const { Pool } = require("pg");
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
+    user: "postgres",
+    password: "david2468",
+    host: "localhost",
+    port: "5432",
     database: "tododb"
 });
 
