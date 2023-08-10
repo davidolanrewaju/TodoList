037e269c2a280d2217023af74e67198950515f68
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-08-10 13:28:56 +0100

    Create content component for props application

---
+++ client/src/components/Content.tsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import TodoList from "./TodoList";
+
+const Content = () => {
+  const [todos, setTodos] = useState([]);
+
+  //HTTP GET request
+  const getTodos = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/todos");
+      const data = await response.json();
+      setTodos(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  //HTTP DELETE request
+  const removeTodo = async (id: number) => {
+    try {
+      const response = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE",
+      });
+      const data = await response.json();
+      getTodos();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return <TodoList todos={todos} removeTodo={removeTodo} />;
+};
+
+export default Content;
