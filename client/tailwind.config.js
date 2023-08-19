1f527bdcbaef36f8fdaef00628a122443f2a1218
David <davidolanrewaju538@gmail.com>
2023-08-19 22:44:21 +0100

    Customize tailwind screens

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,22 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {},
+    screens: {
+      'sm': '320px',
+      // => @media (min-width: 320px) { ... }
+
+      'md': '420px',
+      // => @media (min-width: 420px) { ... }
+
+      'lg': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'xl': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      '2xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    }
   },
   plugins: [],
 }
