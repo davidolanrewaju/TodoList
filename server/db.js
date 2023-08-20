606858d04af320ee6caa2b345a30e571c4d2caba
David Olanrewaju <davidolanrewaju538@gmail.com>
2023-08-21 00:21:35 +0100

    Server update

--- server/db.js
+++ server/db.js
@@ -3,12 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-    connectionString: process.env.DBConfigLink,
-    // user: process.env.USERNAME,
-    // password: process.env.PASSWORD,
-    // host: process.env.HOST,
-    // port: process.env.DBPORT,
-    // database: "tododb"
+    // connectionString: process.env.DBConfigLink,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    host: process.env.HOST,
+    port: process.env.DBPORT,
+    database: process.env.DATABASE
 });
 
 module.exports = pool;
